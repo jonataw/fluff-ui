b9d59bab8522a5e3c9a724743f83633f6f19162f
Jonatan Wackström <29772763+Asgarv@users.noreply.github.com>
2020-06-16 01:24:17 +0300

    Add Icon

---
+++ src/components/icon/icon.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="icon--root">
+    <i class="icon" :class="`icon-${i}`" />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator';
+
+@Component({})
+export default class extends Vue {
+  @Prop({ type: String, required: true }) i!: string;
+}
+</script>
+
+<style lang="scss" scoped>
+.icon-root {
+  display: inline-block;
+}
+</style>
